48633bcacdfee322dad8fc326f1c03c5c9be06e5
movax <lukasz.hutyra@gmail.com>
2013-01-05 22:12:22 +0100

    reference to Vis.jar added

--- PolygonEstimation/src/PolygonEstimation.java
+++ PolygonEstimation/src/PolygonEstimation.java
@@ -5,9 +5,16 @@
  * Time: 20:46
  * To change this template use File | Settings | File Templates.
  */
+import marathon.tco12onsite.*;
 public class PolygonEstimation {
 
     public int[] estimate(double D, double P)
+    {
+        return null;
+
+    }
+
+    private Object getNextScanCoordinates(Object previousScan)
     {
         return null;
     }
---
+++ PolygonEstimation/src/Starter.java
@@ -0,0 +1,7 @@
+public class Starter {
+    public static void main(java.lang.String[] strings)
+    {
+        System.out.println("hello world");
+    }
+
+}
