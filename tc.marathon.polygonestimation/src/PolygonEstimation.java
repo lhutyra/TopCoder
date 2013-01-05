beb6eb287133974f82cda0ccdfe662c2e3078f19
movax <lukasz.hutyra@gmail.com>
2013-01-06 00:41:43 +0100

    Preparing test enviroment

--- tc.marathon.polygonestimation/src/PolygonEstimation.java
+++ tc.marathon.polygonestimation/src/PolygonEstimation.java
@@ -5,17 +5,64 @@
  * Time: 20:46
  * To change this template use File | Settings | File Templates.
  */
-import marathon.tco12onsite.*;
+
 public class PolygonEstimation {
 
-    public int[] estimate(double D, double P)
-    {
-        return null;
+    /**
+     * public int[] estimate(double D, double P)
+     * {
+     * return null;
+     * <p/>
+     * }
+     */
+
+    public int[] estimate(double D, double P) {
+      /*  # use this method only for local testing
+        # for server testing, use RayCaster.sendRequest() instead
+        sendRequest(x1, y1, x2, y2):
+        printLine(1)
+        printLine(x1)
+        printLine(y1)
+        printLine(x2)
+        printLine(y2)
+
+        flush(stdout)
+
+        res = int(readLine())
+
+        if (res == 0):
+        return No Intersection
+
+        X = double(readLine())
+        Y = double(readLine())
+
+        return Intersection Approximately at (X, Y)  */
+        int[] polygon = new int[20];
+        polygon[0] = 0;
+        polygon[1] = 0;
+        polygon[2] = 110;
+        polygon[3] = 0;
+        polygon[4] = 300;
+        polygon[5] = 0;
+        polygon[6] = 100;
+        polygon[7] = 400;
+        polygon[8] = 330;
+        polygon[9] = 444;
+        polygon[10] = 500;
+        polygon[11] = 400;
+        polygon[12] = 600;
+        polygon[13] = 350;
+        polygon[14] = 100;
+        polygon[15] = 300;
+        polygon[16] = 100;
+        polygon[17] = 50;
+        polygon[18] = 20;
+        polygon[19] = 20;
+        return polygon;
 
     }
 
-    private Object getNextScanCoordinates(Object previousScan)
-    {
+    private Object getNextScanCoordinates(Object previousScan) {
         return null;
     }
 }
--- tc.marathon.polygonestimation/src/Starter.java
+++ tc.marathon.polygonestimation/src/Starter.java
@@ -1,7 +1,28 @@
+import java.util.Scanner;
+
 public class Starter {
-    public static void main(java.lang.String[] strings)
-    {
-        System.out.println("hello world");
+    public static void main(java.lang.String[] strings) {
+
+        Scanner inScan = new Scanner(System.in);
+        // String  inScan.nextLine();
+        // marathon.tco12onsite.
+        //System.out.println("1");
+        //System.out.println("0");
+        //System.out.println("0");
+        //System.out.println("300");
+        //System.out.println("300");
+
+        String paramD = inScan.nextLine();
+        String paramP = inScan.nextLine();
+
+        PolygonEstimation polEstInst = new PolygonEstimation();
+        int[] estimationRes = polEstInst.estimate(java.lang.Integer.parseInt(paramD), Integer.parseInt(paramP));
+        System.out.println("2");
+        System.out.println("10");
+         for(int i=0;i<20;i++)
+             System.out.println(estimationRes[i]);
+        System.out.flush();
+
     }
 
 }
