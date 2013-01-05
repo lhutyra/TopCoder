7ba7427c941ebc4e5d9dea4acecad98fe9b14333
movax <lukasz.hutyra@gmail.com>
2013-01-05 20:57:07 +0100

    PolygonEstimation template

---
+++ PolygonEstimation/src/PolygonEstimation.java
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: movax
+ * Date: 05.01.13
+ * Time: 20:46
+ * To change this template use File | Settings | File Templates.
+ */
+public class PolygonEstimation {
+
+    public int[] estimate(double D, double P)
+    {
+        return null;
+    }
+}
